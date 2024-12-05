5718f2c1d0b1a8eb83e73d93e29456fa9b08f8ba
Broslunas <pablo.luna.perez.008@gmail.com>
2024-12-05 09:21:53 +0000

    updates

--- src/pages/api/test.ts
+++
@@ -1,9 +0,0 @@
-// src/pages/api/status.ts
-
-export const get = async () => {
-    return new Response(
-      JSON.stringify({ message: 'API está funcionando' }),
-      { status: 200, headers: { 'Content-Type': 'application/json' } }
-    );
-  };
-  
