b5ba963b2c9f39ababf006989e1581f1ca4d8118
Broslunas <pablo.luna.perez.008@gmail.com>
2024-12-05 09:41:24 +0000

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,26 @@
 import { defineCollection, z } from "astro:content"
 
+const work = defineCollection({
+  type: "content",
+  schema: z.object({
+    company: z.string(),
+    role: z.string(),
+    dateStart: z.coerce.date(),
+    dateEnd: z.union([z.coerce.date(), z.string()]),
+  }),
+})
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    summary: z.string(),
+    date: z.coerce.date(),
+    tags: z.array(z.string()),
+    draft: z.boolean().optional(),
+  }),
+})
+
 const projects = defineCollection({
   type: "content",
   schema: z.object({
@@ -21,4 +42,4 @@ const legal = defineCollection({
   }),
 })
 
-export const collections = { projects, legal }
+export const collections = { work, blog, projects, legal }
