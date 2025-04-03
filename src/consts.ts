ba59ea4c02e88275af051517f0410b4aba649281
Broslunas0 <pablo.luna.perez.008@gmail.com>
2025-04-03 15:31:32 +0100

    feat: update site title and add localized page titles for better navigation

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Broslunas | Portfolio';
+export const SITE_TITLE = 'Broslunas';
 export const SITE_TAGLINE = ''; // site tagline
 export const SITE_DESCRIPTION = ''; // site meta description for SEO
+
+export const TITLE_HOME = 'Inicio';
+export const TITLE_CONTACT = 'Contacto';
+export const TITLE_ABOUT = 'Sobre mí';
+export const TITLE_PROJECTS = 'Proyectos';
+export const TITLE_POSTS = 'Publicaciones';
