0d8b02a29446b871f0c9729eebfad7ff33fce911
Broslunas0 <pablo.luna.perez.008@gmail.com>
2025-04-03 16:09:37 +0100

    feat: enhance project list with cover images and improve layout

--- src/pages/proyectos/projects.ts
+++ src/pages/proyectos/projects.ts
@@ -8,29 +8,60 @@ export interface Project {
 	postLink?: string;
 	demoLinkRel?: string;
 	[key: string]: any;
+	cover?: string;
 }
 
 export const projects: Project[] = [
 	{
 		...(await getRepositoryDetails('broslunas/spotify-stats')),
 		name: 'Spotify Stats',
-		description: 'Un sitio web para ver tus estadísticas de Spotify.',
+		description: 'Un sitio web para visualizar estadísticas personalizadas de Spotify, como tus canciones y artistas más escuchados.',
 		demoLink: 'https://stats.broslunas.com',
 		postLink: '/spotify-stats',
-		tags: ['Astro', 'Javascript']
+		tags: ['Astro', 'Javascript'],
+		cover: '/src/assets/img/posts/spotify-stats.webp'
+	},
+	{
+		...(await getRepositoryDetails('broslunas/games')),
+		name: 'Games',
+		description: 'Una colección de juegos interactivos desarrollados para la web.',
+		demoLink: 'https://games.broslunas.com',
+		postLink: '/games',
+		tags: ['Astro', 'Javascript', 'HTML', 'CSS'],
+		cover: '/src/assets/img/posts/games.webp'
+	},
+	{
+		...(await getRepositoryDetails('broslunas/clock')),
+		name: 'Clock',
+		description: 'Aplicación web para crear temporizadores y cronómetros personalizados.',
+		demoLink: 'https://clock.broslunas.com',
+		postLink: '/clock',
+		tags: ['HTML', 'CSS', 'Javascript'],
+		cover: '/src/assets/img/posts/clock.webp'
+	},
+	{
+		...(await getRepositoryDetails('broslunas/Calculator')),
+		name: 'Calculadora',
+		description: 'Calculadora web sencilla para realizar operaciones matemáticas básicas.',
+		demoLink: 'https://calc.broslunas.com',
+		postLink: '/calc',
+		tags: ['HTML', 'CSS', 'Javascript'],
+		cover: '/src/assets/img/posts/calc.webp'
 	},
 	{
 		name: 'Videos',
-		description: 'Sitio web para poder ver tus series y películas favoritas.',
+		description: 'Plataforma web para disfrutar de tus series y películas favoritas en un solo lugar.',
 		demoLink: 'https://videos.broslunas.com',
 		postLink: '/videos',
-		tags: ['Astro', 'Markdown']
+		tags: ['Astro', 'Markdown', 'Typescript'],
+		cover: '/src/assets/img/posts/brl-videos.webp'
 	},
 	{
 		...(await getRepositoryDetails('broslunas/portfolio')),
 		name: 'Portfolio',
-		description: 'Mi portfolio personal, donde muestro mis proyectos y habilidades.',
+		description: 'Mi portfolio personal donde presento mis proyectos, habilidades y experiencia profesional.',
 		demoLink: 'https://broslunas.com',
-		tags: ['Astro', 'Typescript']
+		tags: ['Astro', 'Typescript'],
+		cover: '/src/assets/img/posts/portfolio.webp'
 	}
 ];
